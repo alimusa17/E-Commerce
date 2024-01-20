0496dde55be6c8bca315b54b80f3ac44ba0fb046
Ali Musa <alimusa_@abv.bg>
2024-01-20 12:30:09 +0200

    Added function: add to cart, button disabled and etc.

--- js/glide.js
+++ js/glide.js
@@ -1,9 +1,11 @@
+const productsContainer = document.getElementById("product-list");
+
 export function product1(){
-    const config = {
-        type: "carousel",
+    const config = {        
         perView: 4,
         gap: 20,
-    /*    autoplay: 3000, */
+        /*    autoplay: 3000, */
+        bound: true,
         breakpoints: {
             992:{
                 perView: 3,
@@ -17,14 +19,15 @@ export function product1(){
         },
     };
     
-    new Glide(".product-carousel", config).mount(); 
+    productsContainer && new Glide(".product-carousel", config).mount(); 
 }
 
 const config2 = {
     type: "carousel",
     perView: 4,
     gap: 20,
-/*    autoplay: 3000, */
+    /*    autoplay: 3000, */
+    bound: true,
     breakpoints: {
         992:{
             perView: 3,
@@ -38,4 +41,4 @@ const config2 = {
     },
 };
 
-new Glide(".product-carousel2", config2).mount();
+productsContainer && new Glide(".product-carousel2", config2).mount();
